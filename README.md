c16c17174e00ababa50a1c86865f17fd5088c90a
Godmar Back <godmar@gmail.com>
2019-09-09 01:18:23 -0400

    reproducible MDX parsing bug

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Install with `yarn`
+
+Then try `yarn gatsby develop`
+
+Look at lecture/lecture4.mdx
+
---
+++ doczrc.js
@@ -0,0 +1,8 @@
+// as per https://www.docz.site/docs/project-configuration
+
+export default {
+    title: "Bug Test",
+    menu: [
+        'MenuEntry1'
+    ]
+}
